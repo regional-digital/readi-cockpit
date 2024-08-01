990b8d67113e60147991c3e613572db03733d8a8
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2024-08-01 21:53:23 +0200

    benutzer wurde nicht angezeigt in der waitingforjoin-mail

--- resources/views/mail/userwaitingforjoin.blade.php
+++ resources/views/mail/userwaitingforjoin.blade.php
@@ -1,7 +1,7 @@
 <x-mail::message>
 # Benutzer wartet auf Beitritt
 
-{{ $groupmember->group->name }} wartet darauf, der Gruppe {{ $groupmember->group->name }} beizutreten.
+{{ $groupmember->email }} wartet darauf, der Gruppe {{ $groupmember->group->name }} beizutreten.
 
 <x-mail::button :url="$url" color="success">
     Gruppe aufrufen
