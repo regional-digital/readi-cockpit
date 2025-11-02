76be88edbd8c3d6310958ff3ed12c166800be292
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2025-11-02 21:42:10 +0000

    updated keycloak sso plugin

--- config/filament-keycloak-sso.php
+++ config/filament-keycloak-sso.php
@@ -28,6 +28,23 @@ return [
     'keycloak_client_secret' => env('KEYCLOAK_CLIENT_SECRET', 'client_secret'),
 
     /**
+     * Keycloak roles level, can be:
+     * - "realm" the plugin search the role inside the roles assigned in the realm level
+     * - "client" the plugin search the role inside the roles assigned in the client level
+     * - "both" the plugin search the role inside the roles assigned in both the realm and the client level
+     */
+    'roles_level' => env('KEYCLOAK_ROLES_LEVEL', 'realm'),
+
+    /**
+     * Keycloak resource client
+     * If the "roles_level" is defined to "client" or "both" this parameter is user
+     * to define the client for the 'resource_access' to get roles
+     *
+     * by default this value equals to keycloak client id
+     */
+    'keycloak_resource_client' => env('KEYCLOAK_RESOURCE_CLIENT', env('KEYCLOAK_CLIENT_ID')),
+
+    /**
      * Profile page configurations
      */
     'profile' => [
