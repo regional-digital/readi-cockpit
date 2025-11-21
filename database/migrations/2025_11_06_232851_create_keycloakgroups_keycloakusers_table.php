299fd549ff7eb3bb07eacac25b389233d8a4ede9
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2025-11-21 19:07:07 +0000

    Fehler in Migration, Create statt alter

--- database/migrations/2025_11_06_232851_create_keycloakgroups_keycloakusers_table.php
+++ database/migrations/2025_11_06_232851_create_keycloakgroups_keycloakusers_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('keycloakgroups_keycloakusers', function (Blueprint $table) {
+        Schema::create('keycloakgroups_keycloakusers', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->foreignIdFor(Keycloakgroup::class);
