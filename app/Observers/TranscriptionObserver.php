165f8df85d83d6d86bc80bda1cc44d76b059b087
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2026-05-14 12:16:38 +0000

    evtl liegengebliebene Sachen löschen

--- app/Observers/TranscriptionObserver.php
+++ app/Observers/TranscriptionObserver.php
@@ -13,6 +13,8 @@ class TranscriptionObserver
             $basename = basename($transcription->attachment, '.mp3');
             if(Storage::exists($transcription->attachment)) Storage::delete($transcription->attachment);
             if(Storage::exists($basename.'.zip')) Storage::delete($basename.'.zip');
+            if(Storage::exists("output/".$basename.'.mp3')) Storage::delete("output/".$basename.'.mp3');
+            if(Storage::exists("input/".$basename.'.zip')) Storage::delete("input/".$basename.'.zip');
         }
     }
 }
